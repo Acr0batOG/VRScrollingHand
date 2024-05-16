d24a448ea2783dd2b450e9ff1529730aca2eccd3
Acr0batOG <grhys2001@gmail.com>
2024-05-16 09:40:34 -0700

    Added an all variable class to change selection method

---
+++ Assets/Scripts/AllVariables.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AllVariables : MonoBehaviour
+{
+    public int techniqueNumber = -1;
+    public int areaNumber = -1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/ArmUIController.cs
+++ Assets/Scripts/ArmUIController.cs
@@ -11,12 +11,13 @@ public class ArmUIController : MonoBehaviour
     [SerializeField] private Transform endPoint;
     [SerializeField] private ScrollRect scrollableList;
 
+    public AllVariables allVariables;
     private float listLength;
     private Vector3 baseScale;
     [SerializeField] private float scaleFactor = 2f;
-    private int itemsInList = 10;
-    private Vector3 initialScrollPosition;
-    [SerializeField] private float scrollSpeed = 25f;
+    // private int itemsInList = 10;
+     private Vector3 initialScrollPosition;
+    [SerializeField] private float scrollSpeed = 50f;
 
     public TextMeshPro distText;
 
@@ -37,19 +38,33 @@ public class ArmUIController : MonoBehaviour
     {
         selectText.text = "Enter";
         baseScale = other.transform.localScale;
-        ScrollList(other);
+        if(allVariables.techniqueNumber==1){
+            staticScrollList(other);
+        }else if(allVariables.techniqueNumber==2){
+            pointScrollList(other);
+        }else if(allVariables.techniqueNumber==3){
+            dynamicScrollList(other);
+        }
+
     }
     
 
     private void OnTriggerStay(Collider other)
-    {   
-        ScrollList(other);
+    {   if(allVariables.techniqueNumber==1){
+        staticScrollList(other);
+        }else if(allVariables.techniqueNumber==2){
+            pointScrollList(other);
+        }else if(allVariables.techniqueNumber==3){
+            dynamicScrollList(other);
+        }
     }
 
     private void OnTriggerExit(Collider other)
     {   
         selectText.text = "Exit";
+       
         baseScale = other.transform.localScale;
+        
     
     }
 
@@ -68,8 +83,7 @@ public class ArmUIController : MonoBehaviour
     //     distText.text = "Position:" + contactPoint.ToString();
     // }
 
-    private void ScrollList(Collider collisionInfo)
-{
+    private void staticScrollList(Collider collisionInfo){
     // Determine the contact point
     Vector3 contactPoint = collisionInfo.ClosestPoint(startPoint.position);
 
@@ -101,6 +115,75 @@ public class ArmUIController : MonoBehaviour
     scrollableList.content.anchoredPosition = newScrollPosition;
 
     // Update the distance text
-    distText.text = "Position: " + contactPoint.ToString() + " " + newScrollPosition.y.ToString();
-}
+    distText.text = "Static Scroll: Position " + contactPoint.ToString() + " " + newScrollPosition.y.ToString();
+    }
+    private void pointScrollList(Collider collisionInfo){
+        Vector3 contactPoint = collisionInfo.ClosestPoint(startPoint.position);
+
+        // Calculate the middle point between startPoint and endPoint
+        Vector3 middlePoint = (startPoint.position + endPoint.position) / 2f;
+
+        // Calculate the distance from the contact point to the start and end points
+        float distanceFromStart = (contactPoint - startPoint.position).magnitude;
+        float distanceFromEnd = (contactPoint - endPoint.position).magnitude;
+
+        // Determine the polarity based on which end the contact point is closer to
+        int polarity = distanceFromStart < distanceFromEnd ? -1 : 1;
+
+        // Get the content height and the viewport height
+        float contentHeight = scrollableList.content.sizeDelta.y;
+        float viewportHeight = scrollableList.viewport.rect.height;
+
+        // Determine the total number of bins (1-20)
+        int totalBins = 20;
+
+        // Calculate the position along the arm in terms of the bin index (1-20)
+        float armLength = (endPoint.position - startPoint.position).magnitude;
+        float contactPosition = (contactPoint - startPoint.position).magnitude;
+        int binIndex = Mathf.Clamp(Mathf.RoundToInt((contactPosition / armLength) * totalBins), 1, totalBins);
+
+        // Determine the new scroll position based on the bin index
+        float binHeight = (contentHeight - viewportHeight) / (totalBins - 1);
+        float newScrollPositionY = (binIndex - 1) * binHeight;
+
+        // Set the new anchored position for the scroll content
+        Vector2 newScrollPosition = new Vector2(scrollableList.content.anchoredPosition.x, newScrollPositionY);
+        scrollableList.content.anchoredPosition = newScrollPosition;
+
+        // Update the distance text
+        distText.text = "Point Scroll: Position " + contactPoint.ToString() + " " + newScrollPosition.y.ToString();
+    }
+    private void dynamicScrollList(Collider collisionInfo){
+        Vector3 contactPoint = collisionInfo.ClosestPoint(startPoint.position);
+
+        // Calculate the middle point between startPoint and endPoint
+        Vector3 middlePoint = (startPoint.position + endPoint.position) / 2f;
+
+        // Calculate the distance from the contact point to the start and end points
+        float distanceFromStart = (contactPoint - startPoint.position).magnitude;
+        float distanceFromEnd = (contactPoint - endPoint.position).magnitude;
+
+        // Determine the polarity based on which end the contact point is closer to
+        int polarity = distanceFromStart < distanceFromEnd ? -1 : 1;
+
+        // Get the content height and the viewport height
+        float contentHeight = scrollableList.content.sizeDelta.y;
+        float viewportHeight = scrollableList.viewport.rect.height;
+
+        // Calculate the new scroll position based on the distance from the middle point
+        float deltaY = (contactPoint - middlePoint).magnitude * polarity * scrollSpeed;
+
+        // Update the new scroll position
+        Vector2 newScrollPosition = scrollableList.content.anchoredPosition;
+        newScrollPosition.y += deltaY;
+
+        // Clamp the new scroll position within the scrollable area
+        newScrollPosition.y = Mathf.Clamp(newScrollPosition.y, 0, contentHeight - viewportHeight);
+
+        // Set the new anchored position for the scroll content
+        scrollableList.content.anchoredPosition = newScrollPosition;
+
+        // Update the distance text
+        distText.text = "Dynamic Scroll: Position " + contactPoint.ToString() + " " + newScrollPosition.y.ToString();
+    }
 }
