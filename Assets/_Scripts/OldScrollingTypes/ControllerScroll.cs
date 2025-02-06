eb23c40d24c2db7f92ea666e83a16b9437765916
Rhys Smith <grhys2001@gmail.com>
2025-02-06 14:24:07 -0800

    Updated data tracking values

--- Assets/_Scripts/OldScrollingTypes/ControllerScroll.cs
+++ Assets/_Scripts/OldScrollingTypes/ControllerScroll.cs
@@ -40,6 +40,7 @@ namespace _Scripts.OldScrollingTypes
                     {
                         numberOfFlicks++;  // Count the flick
                         isJoystickPushed = false; // Reset state
+                        gameManager.NumberOfFlicks = numberOfFlicks;
                     }
 
                     // If joystick is pushed, accumulate swipe amplitude and mark state
@@ -47,6 +48,7 @@ namespace _Scripts.OldScrollingTypes
                     {
                         totalAmplitudeOfSwipe += Mathf.Abs(verticalInput);
                         isJoystickPushed = true;
+                        gameManager.TotalAmplitudeOfSwipes = totalAmplitudeOfSwipe;
                     }
 
                     // Scroll content
